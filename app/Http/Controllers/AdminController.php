4602879c815ac331792bf132ea83ad033ca96ceb
yaad33va <denisa.valinciute@ktu.edu>
2025-11-30 15:50:01 +0200

    librarian registration errors fixed

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -35,11 +35,22 @@ class AdminController extends Controller
      */
     public function storeLibrarian(Request $request)
     {
+        // Pirmas masyvas - taisyklės
+        // Antras masyvas - jūsų lietuviški pranešimai
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'surname' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ], [
+            'name.required' => 'Vardo laukas yra privalomas.',
+            'surname.required' => 'Pavardės laukas yra privalomas.',
+            'email.required' => 'El. pašto adresas yra privalomas.',
+            'email.email' => 'Įveskite galiojantį el. pašto adresą.',
+            'email.unique' => 'Toks el. paštas jau registruotas sistemoje.',
+            'password.required' => 'Slaptažodis yra privalomas.',
+            'password.confirmed' => 'Slaptažodžiai nesutampa.', // Čia dažniausia klaida, kai įveda neteisingai antrą kartą
+            'password.min' => 'Slaptažodis turi būti bent :min simbolių ilgio.',
         ]);
 
         $user = User::create([
--- resources/views/admin/create-librarian.blade.php
+++ resources/views/admin/create-librarian.blade.php
@@ -6,37 +6,56 @@
     <div class="card" style="max-width: 600px; margin: 2rem auto;">
         <h2 style="text-align: center; margin-bottom: 1.5rem;">Naujo bibliotekininko informacija</h2>
 
-        <form method="POST" action="{{ route('admin.librarian.store') }}">
+        {{-- PAKEITIMAS: Pridėtas 'novalidate' atributas, kad naršyklė nerodytų savo angliškų pranešimų --}}
+        <form method="POST" action="{{ route('admin.librarian.store') }}" novalidate>
             @csrf
 
             <!-- Name -->
             <div class="form-group">
                 <label for="name">Vardas</label>
-                <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus autocomplete="name">
+                {{-- PAKEITIMAS: Pridėta klasė is-invalid, jei yra klaida --}}
+                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autofocus autocomplete="name">
+
+                {{-- PAKEITIMAS: Klaidų atvaizdavimas --}}
+                @error('name')
+                <span class="text-danger" style="font-size: 0.875em;">{{ $message }}</span>
+                @enderror
             </div>
 
-            <!-- Name -->
+            <!-- Surname -->
             <div class="form-group">
-                <label for="name">Pavardė</label>
-                <input id="surname" type="text" name="surname" value="{{ old('surname') }}" required autofocus autocomplete="surname">
+                <label for="surname">Pavardė</label>
+                <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autofocus autocomplete="surname">
+
+                @error('surname')
+                <span class="text-danger" style="font-size: 0.875em;">{{ $message }}</span>
+                @enderror
             </div>
 
             <!-- Email Address -->
             <div class="form-group">
                 <label for="email">El. paštas</label>
-                <input id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="email">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                @error('email')
+                <span class="text-danger" style="font-size: 0.875em;">{{ $message }}</span>
+                @enderror
             </div>
 
             <!-- Password -->
             <div class="form-group">
                 <label for="password">Slaptažodis</label>
-                <input id="password" type="password" name="password" required autocomplete="new-password">
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                @error('password')
+                <span class="text-danger" style="font-size: 0.875em;">{{ $message }}</span>
+                @enderror
             </div>
 
             <!-- Confirm Password -->
             <div class="form-group">
                 <label for="password_confirmation">Patvirtinkite slaptažodį</label>
-                <input id="password_confirmation" type="password" name="password_confirmation" required autocomplete="new-password">
+                <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
             </div>
 
             <div style="display: flex; justify-content: flex-end; align-items: center; margin-top: 1.5rem;">
